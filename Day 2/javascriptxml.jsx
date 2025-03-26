fcd4729ec1776b1a8e98125765ac1b773c952574
lilacdotdev <talk@lilaccs.dev>
2025-03-26 16:37:05 -0400

    Begin Day 2 - Prepare for project structure

---
+++ Day 2/javascriptxml.jsx
@@ -0,0 +1,10 @@
+// Day 2 - JSX
+
+// Creating elements in jsx
+const title = <h1>Title Text</h1>
+const header = (
+    <header>
+        {title}
+        <p>This is a header yes yes wow</p>
+    </header>
+)
